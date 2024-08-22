6b1d9b91f70f5d08b1448193a07c8b0e273882ca
ValeriaSeminerio <95383310+ValeriaSeminerio@users.noreply.github.com>
2024-08-22 12:27:27 +0200

    fine

--- UI/controller.py
+++ UI/controller.py
@@ -1,12 +1,73 @@
 import flet as ft
 
-
 class Controller:
     def __init__(self, view, model):
         # the view, with the graphical elements of the UI
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        self._currentCountry = None
 
     def handleCalcola(self, e):
-        pass
+        year = self._view._txtAnno.value
+        try:
+            yearN = int(year)
+        except ValueError:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("Perfavore inserisci un valore numerico"))
+            self._view.update_page()
+            return
+
+        if yearN<1816 or yearN>2016:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("Inserisci un anno compreso tra il 1816 e il 2016"))
+            self._view.update_page()
+            return
+
+        self._model.builGraph(yearN)
+
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text("Grafo correttamente creato."))
+        self._view._txt_result.controls.append(ft.Text(f"Il grafo ha {self._model.getNumCompConnesse()} componenti connesse."))
+        self._view._txt_result.controls.append(ft.Text("Di seguito il dettaglio sui nodi"))
+
+
+        for n in self._model.getNodes():
+            self._view._txt_result.controls.append(
+                ft.Text(f"{n} -- {self._model.getNumConfinanti(n)} vicini."))
+
+        self._view._ddStato.disabled = False
+        self._view._btnRaggiungibili.disabled = False
+
+        self._fillDD()
+        self._view.update_page()
+
+    def handleRaggiungibili(self, e):
+        raggiungibili = self._model.getRaggiungibili(self._currentCountry)
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text(f"Da{self._currentCountry} è possibile raggiungere a piedi {len(raggiungibili)} stati: "))
+        for r in raggiungibili:
+            self._view._txt_result.controls.append(
+                ft.Text(f"{r}"))
+
+        self._view.update_page()
+
+
+    def _fillDD(self):
+        allStati = self._model.getNodes()
+
+        for s in allStati:
+            self._view._ddStato.options.append(ft.dropdown.Option(text=s.StateNme,
+                                              data = s,
+                                              on_click = self._read_DD_Stato))
+
+    def _read_DD_Stato(self, e):
+        print("funzione leggi stato chiamata")
+        if e.control.data is None:
+            self._currentCountry = None
+        else:
+            self._currentCountry = e.control.data
+
+        print(self._currentCountry)
+
+
--- model/model.py
+++ model/model.py
@@ -1,3 +1,4 @@
+from datetime import time, datetime
 import networkx as nx
 
 from database.DAO import DAO
@@ -6,5 +7,98 @@ from database.DAO import DAO
 class Model:
 
     def __init__(self):
-        pass
+        self._graph = nx.Graph()
 
+    def builGraph(self, year):
+        self._countries = DAO.getAllCountries(year)
+        self._idMap = {}
+        for c in self._countries:
+            self._idMap[c.CCode] = c
+
+        self._graph.clear()
+        borders = DAO.getCountryPairs(self._idMap, year)
+        self._graph.add_nodes_from(self._countries)
+        for b in borders:
+            self._graph.add_edge(b.c1, b.c2)
+
+    def getNodes(self):
+        return list(self._graph.nodes)
+
+    def getNumConfinanti(self, v):
+        return len(list(self._graph.neighbors(v)))
+
+    def getNumCompConnesse(self):
+        return nx.number_connected_components(self._graph)
+
+    def getRaggiungibili(self, n):
+        tic = datetime.now()
+        a = self.getRaggiungibiliDFS(n)
+        print (f"DFS: {datetime.now()-tic} - {len(a)}")
+
+        tic = datetime.now()
+        b = self.getRaggiungibiliBFS(n)
+        print(f"BFS: {datetime.now() - tic} - {len(b)}")
+
+        tic = datetime.now()
+        c= self.getRaggiungibiliIterative(n)
+        print(f"Iterative: {datetime.now() - tic} - {len(c)}")
+
+        tic = datetime.now()
+        d = self.getRaggiungibiliRecursive(n)
+        print(f"Recursive: {datetime.now() - tic} - {len(d)}")
+
+        return a
+
+
+
+    def getRaggiungibiliDFS(self, n):
+        tree = nx.dfs_tree(self._graph, n) #solo archi dell'albero e archi all'indietro
+        a = list(tree.nodes)
+        a.remove(n)
+        return a
+
+    def getRaggiungibiliBFS(self, n):
+        tree = nx.bfs_tree(self._graph, n) #archi dell'albero e archi trasversali
+        a = list(tree.nodes)
+        a.remove(n)
+        return a
+
+    #dfs(G)=bfs(G) solo se contengono solo archi dell'albero di visita e quindi g è esso stesso un albero
+
+    def getRaggiungibiliIterative(self, n):
+        from collections import deque
+
+        visited = []
+        toBeVisited = deque()
+
+        visited.append(n)
+
+        toBeVisited.extend(self._graph.neighbors(n))
+
+        while toBeVisited:
+            temp = toBeVisited.popleft()
+            visited.append(temp)
+
+            neighbors =list(self._graph.neighbors(temp))
+
+            neighbors = [neighbor for neighbor in neighbors if neighbor not in visited]
+
+            neighbors = [neighbor for neighbor in neighbors if neighbor not in toBeVisited]
+
+            toBeVisited.extend(neighbors)
+
+        visited.remove(n)
+        return  visited
+
+    def getRaggiungibiliRecursive(self, n):
+        visited = []
+        self._recursive_visit(n, visited)
+        visited.remove(n)
+        return visited
+
+    def _recursive_visit(self, n, visited):
+        visited.append(n)
+
+        for c in self._graph.neighbors(n):
+            if c not in visited:
+                self._recursive_visit(c, visited)
